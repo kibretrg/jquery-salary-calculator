1b3bec3379e2feb80e50ee087aba56e376a78b62
kibretrg <kibret_g@yahoo.com>
2019-03-10 17:53:25 -0500

    employees input working

--- client.js
+++ client.js
@@ -1,5 +1,27 @@
 console.log('jquery-salary-calculator');
 
-$(document).ready(readyNow);{
-    console.log('in readyNow');
-}
+$( document ).ready(readyNow)
+
+
+let employees = [];
+
+function addItem(){
+    console.log('in addItem');
+    
+    let newEmployee = {
+    firstName: $('#firstNameIn').val(),
+    lastName: $('#lastNameIn').val(),
+    idNumber: $('#idNumberIn').val(),
+    jobTitle: $('#jobTitleIn').val(),
+    annualSalary: $('#annualSalaryIn').val()
+    }// end newEmployee
+    employees.push(newEmployee);
+    console.log('employees', employees);
+    
+}//end addItem
+
+
+function readyNow(){
+    $('#submitButton').on('click', addItem)
+    
+}//end readyNow
