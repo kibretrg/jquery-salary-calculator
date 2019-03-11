955122aa812109026d5b3f6ebc3a314b099bda38
kibretrg <kibret_g@yahoo.com>
2019-03-10 22:12:43 -0500

    added table

--- client.js
+++ client.js
@@ -2,37 +2,32 @@ console.log('jquery-salary-calculator');
 
 $( document ).ready(readyNow)
 
-
-let employeeArray = [];
-
-function addItem(){
-    console.log('in addItem');
-    
-    let eachEmployee = {
-    firstName = $('#firstNameIn').val(),
-    lastName = $('#lastNameIn').val(),
-    idNumber = $('#idNumberIn').val(),
-    jobTitle = $('#jobTitleIn').val(),
-    annualSalary = $('#annualSalaryIn').val()
-    }// end newEmployee
-    employeeArray.push(eachEmployee);
-    console.log('employeeArray', employeeArray);
-    displayEmployee();
-}//end addItem
-
-function displayEmployee(){
-    console.log('in displayEmployee');
-    let employeeOut = $('outputDiv');
-    employeeOut.empty();
-    //loop through the employee array
-    for(let employee of employeeArray){
-    //for each item create a new <li> in employeeOut
-        employeeOut.append(`<li>${employee.firstName}</li>`)
-    }
-    
-}// end displayEmployee
-
 function readyNow(){
     $('#submitButton').on('click', addItem)
     
 }//end readyNow
+
+
+
+function addItem(){
+    console.log('in addItem');
+    
+  
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNumber = $('#idNumberIn').val();
+    let jobTitle = $('#jobTitleIn').val();
+    let annualSalary = $('#annualSalaryIn').val();
+
+    let employeeOut = $('#employeeOut');
+    // employeeOut.empty();
+
+    employeeOut.append(`<tr><td>${firstName}</td><td>${lastName}</td></tr>`)
+
+}//end addItem
+
+function displayEmployee(){
+    console.log('in displayEmployee');
+   
+}// end displayEmployee
+
